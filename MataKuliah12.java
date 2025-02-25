1013055052e1de40859e4aca24352c19f251ede8
meryymarta <meriammartadinata@gmail.com>
2025-02-25 16:42:51 +0700

    3.4 Constructor Berparameter setelah pertanyaan 2-4

--- MataKuliah12.java
+++ MataKuliah12.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class MataKuliah12 {
     public String kode;
     public String nama;
@@ -10,5 +10,24 @@ public class MataKuliah12 {
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+    public void tambahData() {
+        Scanner input12 = new Scanner (System.in);
+        System.out.print("Kode         : ");
+        this.kode = input12.nextLine();
+        System.out.print("Nama         : ");
+        this.nama = input12.nextLine();
+        System.out.print("Sks          : ");
+        this.sks = Integer.parseInt(input12.nextLine());
+        System.out.print("Jumlah Jam   :");
+        this.jumlahJam = Integer.parseInt(input12.nextLine());
+        System.out.println("----------------------------");
+    }
+    public void cetakInfo() {
+        System.out.println("Kode         : " + this.kode);
+        System.out.println("Nama         : " + this.nama);
+        System.out.println("Sks          : " + this.sks);
+        System.out.println("Jumlah Jam   : " + this.jumlahJam);
+        System.out.println("---------------------------------");
     }
 }
--- MataKuliahDemo12.java
+++ MataKuliahDemo12.java
@@ -5,30 +5,47 @@ public class MataKuliahDemo12 {
         MataKuliah12[] arrayOfMatakuliah = new MataKuliah12[3];
         String kode, nama, dummy;
         int sks, jumlahJam;
-        
-        for(int i = 0; i < 3; i++){   
-        System.out.println("Masukkan Data MataKuliah ke- " + (i + 1));
-        System.out.print("Kode       : ");
-        kode = input12.nextLine();
-        System.out.print("Nama       : ");
-        nama = input12.nextLine();
-        System.out.print("Sks        : ");
-        dummy = input12.nextLine();
-        sks = Integer.parseInt(dummy);
-        System.out.print("Jumlah Jam : ");
-        dummy = input12.nextLine();
-        jumlahJam = Integer.parseInt(dummy);
-        System.out.println("------------------------------------");
-        
-        arrayOfMatakuliah[i] = new MataKuliah12(kode, nama, sks, jumlahJam);
-    }
-        for(int i = 0; i < 3; i++) {
-        System.out.println("Kode           : "+ arrayOfMatakuliah[i].kode);
-        System.out.println("Nama           : "+ arrayOfMatakuliah[i].nama);
-        System.out.println("Sks            : "+ arrayOfMatakuliah[i].sks);
-        System.out.println("Jumlah Jam     : "+ arrayOfMatakuliah[i].jumlahJam);
-        System.out.println("-----------------------------------");
 
+        System.out.print("Masukkan Jumlah MataKuliah: ");
+        int jumlahMatkul = input12.nextInt();
+        input12.nextLine();
+
+        MataKuliah12[] arrayOfMataKuliah12 = new MataKuliah12[jumlahMatkul];
+        
+        for (int i = 0; i < jumlahMatkul; i++) {
+            System.out.println("Masukkan Data MataKuliah ke-" + (i + 1));
+            arrayOfMatakuliah[i] = new MataKuliah12("", "", 0, 0);
+            arrayOfMatakuliah[i].tambahData();
         }
-     }
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data MataKuliah ke-" + (i + 1));
+            arrayOfMatakuliah[i].cetakInfo();
+        } 
+    }
 }
+
+// for(int i = 0; i < 3; i++){   
+// System.out.println("Masukkan Data MataKuliah ke- " + (i + 1));
+// System.out.print("Kode       : ");
+// kode = input12.nextLine();
+// System.out.print("Nama       : ");
+// nama = input12.nextLine();
+// System.out.print("Sks        : ");
+// dummy = input12.nextLine();
+// sks = Integer.parseInt(dummy);
+// System.out.print("Jumlah Jam : ");
+// dummy = input12.nextLine();
+// jumlahJam = Integer.parseInt(dummy);
+// System.out.println("------------------------------------");
+
+// arrayOfMatakuliah[i] = new MataKuliah12(kode, nama, sks, jumlahJam);
+// }
+// for(int i = 0; i < jumlahMatkul; i++) {
+// System.out.println("Kode           : "+ arrayOfMatakuliah[i].kode);
+// System.out.println("Nama           : "+ arrayOfMatakuliah[i].nama);
+// System.out.println("Sks            : "+ arrayOfMatakuliah[i].sks);
+// System.out.println("Jumlah Jam     : "+ arrayOfMatakuliah[i].jumlahJam);
+// System.out.println("-----------------------------------");
+
+// }
